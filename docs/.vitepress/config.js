5518f9b0f628569774dd2a8c5fe7f68d5c51da00
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2022-11-29 15:35:06 +0000

    v1

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+export default {
+    title: 'icepcp docs',
+    description: 'Documentation for all things icepcp',
+    themeConfig: {
+      nav: [
+        { text: 'test', link: '/test/index' }
+      ]
+    }
+  }
